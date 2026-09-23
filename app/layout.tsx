6682927c8e3f7716a99af396ef4ecb8e75296c03
muhsinswaleh69-max <muhsinswaleh69@gmail.com>
2026-09-23 13:25:32 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,1 @@
-export const metadata = { title: "ShulePulse Pro - Senior" }
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <html lang="en"><body style={{margin:0}}>{children}</body></html>
-}
+
